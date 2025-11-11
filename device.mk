abc958e5da3bb3a13c2ec03544d03c4b36df8440
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-11 13:13:52 +0800

    virtio_arm64: Move the variables before inherit-product down on device.mk
    inherit-product doesn't include the makefile immediately, but appends
    it to the include stack, so the variables gets set at anywhere on
    the current makefile will be available on the inherited makefile too
    
    Change-Id: I4136ffd4d8df376b335feb4c6236584c62b90265

--- device.mk
+++ device.mk
@@ -3,13 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Kernel (Prebuilt)
-TARGET_PREBUILT_KERNEL_ARCH := arm64
-
 # Inherit from common
 $(call inherit-product, device/virt/virtio-common/device-common.mk)
 
 DEVICE_PATH := device/virt/virtio_arm64
+
+# Kernel (Prebuilt)
+TARGET_PREBUILT_KERNEL_ARCH := arm64
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
