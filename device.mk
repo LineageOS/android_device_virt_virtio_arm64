89322539d42c28e46de555a7cc90930f318fac93
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-11 12:32:27 +0800

    virtio_arm64: Remove support for using prebuilt emulator kernels
    It's been untested and unmaintained for some long time,
    also there's likely no need for it anymore
    
    Change-Id: Ic140582d6d60098941ce4f8fbfd030d12e4701b5

--- device.mk
+++ device.mk
@@ -5,9 +5,7 @@
 #
 
 # Kernel (Prebuilt)
-TARGET_PREBUILT_EMULATOR_KERNEL_USE ?= 6.6
 TARGET_PREBUILT_KERNEL_ARCH := arm64
-TARGET_PREBUILT_KERNEL_MODULES_ARCH := arm64
 
 # Inherit from common
 $(call inherit-product, device/virt/virtio-common/device-common.mk)
